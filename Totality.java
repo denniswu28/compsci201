454f18804e5d1ae5ac3cf89701b95f7397dfe02b
denniswu28 <tw292@duke.edu>
2022-08-31 20:05:28 -0400

    First draft of APT 1.1

---
+++ Totality.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Totality {
+    public int sum(int[] a, String stype){
+        int len = a.length;
+        int number_sum = 0;
+        Map<String, Integer> step = new HashMap<String, Integer>;
+        Map<String, Integer> start = new HashMap<String, Integer>;
+
+        step.put("odd", 2);
+        step.put("even", 2);
+        step.put("all", 1);
+
+        start.put("odd", 1);
+        start.put("even", 0);
+        start.put("all", 0);
+
+        for(int i = start.get(stype); i < len; i+=step.get(stype)){
+            number_sum += a[i];
+        }
+
+
+        return number_sum;
+    }
+}
